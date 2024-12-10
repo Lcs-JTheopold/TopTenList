3f1310ac34306b7e85b5559784936dcfb9012034
Jakob Theopold <jtheopold@lcs.on.ca>
2024-12-10 10:13:58 -0500

    Initial Commit

---
+++ TopTenList/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  TopTenList
+//
+//  Created by Jakob Theopold on 10.12.24.
+//
+
+import Foundation
+
+let TopTenCity = ["Munich","Seoul","Berlin","Busan","Toronto","Peterborough","Lakefield","NewYork","Madrid","Dortmund"]
+
+print (TopTenCity[9])
+
+//Count from 0 to 3
+for i in 0...3 {
+    print(i)
+}
+
+//Print each city
+for citymember in TopTenCity {
+    print(citymember)
+}
+
+
+//Count from 4 to 9
+for i in 4...9 {
+    print(i)
+}
+
+
